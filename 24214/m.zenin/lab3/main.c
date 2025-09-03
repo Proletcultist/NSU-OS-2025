31a4ba72c19a12f997df4428cac8432f7fa36862
Matvey Zenin <m.zenin1@g.nsu.ru>
2025-09-03 21:26:47 +0700

    Done lab3

---
+++ 24214/m.zenin/lab3/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+
+int main(){
+	printf("Real UID: %ju\n", (uintmax_t)getuid());
+	printf("Effective UID: %ju\n\n", (uintmax_t)geteuid());
+
+	FILE *file = fopen("iamfile", "r");
+	if (!file){
+		perror("First try failed");
+	}
+	else{
+		fclose(file);
+	}
+
+	if (seteuid(getuid())){
+		perror("Failed to set EUID to UID");
+		return -1;
+	}
+
+	file = fopen("iamfile", "r");
+	if (!file){
+		perror("Second try failed");
+	}
+	else{
+		fclose(file);
+	}
+
+	return 0;
+}
