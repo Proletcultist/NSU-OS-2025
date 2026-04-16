a316c45570753f9395363ad274a9a2ecc37a81d7
Matvey Zenin <m.zenin1@g.nsu.ru>
2026-04-16 23:31:31 +0700

    test5

--- 24214/m.zenin1/sem2-lab31/src/proxy.c
+++ 24214/m.zenin1/sem2-lab31/src/proxy.c
@@ -1,5 +1,55 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stddef.h>
 #include "aio_scheduler.h"
 
-aio_scheduler_t sched = aio_scheduler_construct();
+static void accept_connection(ssize_t r, int errno, void *udata) {
+    task_t *task = udata;
+
+    struct sockaddr connected_addr;
+    socklen_t connected_len = sizeof(connected_addr);
+    int fd = accept(task->fd, &connected_addr, &connected_len);
+
+    if (connected_addr.sa_family == AF_INET) {
+        char buff[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &((struct sockaddr_in*) &connected_addr)->sin_addr, buff, INET_ADDRSTRLEN);
+        fprintf(stderr, "[Info] Connected: %s\n", buff);
+    }
+}
 
 
+void start_proxy(struct in_addr ip, in_port_t port) {
+    int listening = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addr = {.sin_family = AF_INET,
+                               .sin_port = port,
+                               .sin_addr = ip};
+
+    if (bind(listening, (struct sockaddr*) &addr, sizeof(addr))) {
+        perror("Failed to bind");
+        return;
+    }
+    if (listen(listening, 0)) {
+        perror("Failed to listen");
+        return;
+    }
+
+    task_t accept_task;
+    accept_task = (task_t) {
+                              .type = ACCEPT_CONNECTION_REQUESTS,
+                              .fd = listening,
+                              .buffer = NULL,
+                              .off = 0,
+                              .size = 0,
+                              .data = &accept_task,
+                              .callback = accept_connection
+                           };
+
+    aio_scheduler_schedule(&accept_task);
+
+    while (true) {
+        aio_scheduler_proceed();
+    }
+}
