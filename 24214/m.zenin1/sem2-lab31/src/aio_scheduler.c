21c88c91ce893f45633be6a819ba1cd9bf6b157e
Matvey Zenin <m.zenin1@g.nsu.ru>
2026-04-17 13:49:20 +0700

    test6

--- 24214/m.zenin1/sem2-lab31/src/aio_scheduler.c
+++ 24214/m.zenin1/sem2-lab31/src/aio_scheduler.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <poll.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <errno.h>
 #include "aio_scheduler.h"
 
 aio_scheduler_t sched = {
@@ -54,16 +56,48 @@ void aio_scheduler_schedule(task_t *task) {
     switch (task->type) {
         case ACCEPT_CONNECTION_REQUESTS:
         case READ_REQUEST:
+            desc->reads_amount++;
             fd->events |= POLLIN;
             break;
         case WRITE_REQUEST:
+            desc->writes_amount++;
             fd->events |= POLLOUT;
             break;
     }
 }
 
-static void aio_proceed_tasks(int fd, short revents, tasks_descriptor_t *tasks) {
-    bool writen = false;
+static inline void aio_delete_task(struct pollfd *pollfd, tasks_descriptor_t *tasks, task_t *prev, task_t *this) {
+    // Delete task from linked list
+    prev->next = this->next;
+    
+    // If it was last, prev become last
+    if (tasks->last == this) {
+        tasks->last = prev;
+    }
+
+    switch (this->type) {
+        case ACCEPT_CONNECTION_REQUESTS:
+        case READ_REQUEST:
+            tasks->reads_amount--;
+            break;
+        case WRITE_REQUEST:
+            tasks->writes_amount--;
+            break;
+    }
+
+    if (tasks->reads_amount == 0) {
+        pollfd->events &= ~POLLIN;
+    }
+    if (tasks->writes_amount == 0) {
+        pollfd->events &= ~POLLOUT;
+    }
+    if (tasks->reads_amount == 0 && tasks->writes_amount == 0) {
+        // TODO: Delete fd and descriptor
+    }
+}
+
+static void aio_proceed_tasks(struct pollfd *pollfd, short revents, tasks_descriptor_t *tasks) {
+    bool written = false;
     task_t *prev = tasks->first;
 
     for (task_t *cursor = tasks->first->next; cursor != NULL; cursor = cursor->next) {
@@ -72,10 +106,16 @@ static void aio_proceed_tasks(int fd, short revents, tasks_descriptor_t *tasks) 
                 cursor->callback(0, 0, cursor);
             }
             else if (cursor->type == READ_REQUEST) {
+                aio_delete_task(pollfd, tasks, prev, cursor);
+
+                // Read and callback
+                ssize_t r = read(cursor->fd, cursor->buffer, cursor->size);
+                cursor->callback(r, errno, cursor->data);
             }
         }
         else if (revents & POLLOUT) {
             if (cursor->type == WRITE_REQUEST) {
+                // TODO: Do NONBLOCK write, proceed with task, set written
             }
         }
 
@@ -89,7 +129,7 @@ void aio_scheduler_proceed() {
 
     for (size_t i = 0; i < sched.fds.size; i++) {
         if (sched.fds.arr[i].revents & (POLLIN | POLLOUT)) {
-            aio_proceed_tasks(sched.fds.arr[i].fd, sched.fds.arr[i].revents, &sched.tasks.arr[i]);
+            aio_proceed_tasks(&sched.fds.arr[i], sched.fds.arr[i].revents, &sched.tasks.arr[i]);
         }
         else if (sched.fds.arr[i].revents & (POLLHUP | POLLERR)) {
             // TODO: Delete socket
--- 24214/m.zenin1/sem2-lab31/src/proxy.c
+++ 24214/m.zenin1/sem2-lab31/src/proxy.c
@@ -1,10 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <stddef.h>
 #include "aio_scheduler.h"
+
+#define CHUNK_SIZE 512
+
+typedef struct vector_buffered_task {
+    task_t task;
+
+    void *data;
+    size_t size, cap;
+} vector_buffered_task_t;
+
+static void read_req_line_callback(ssize_t r, int errno, void *udata) {
+    if (r < 0) {
+        // TODO: Handle errors
+    }
+    else if (r == 0) {
+        // TODO: Handle EOF
+    }
+
+    vector_buffered_task_t *task = udata;
+
+    task->size += (size_t) r;
+
+    printf("\nBuffer content (size: %zu):\n", task->size);
+    for (size_t i = 0; i < task->size; i++) {
+        printf("%c", ((char*) task->data)[i]);
+    }
+    printf("\n");
+
+    // TODO: Realloc if buffer exceeded
+    *task = (vector_buffered_task_t)
+            {
+                 .task = {
+                     .type = READ_REQUEST,
+                     .fd = task->task.fd,
+                     .buffer = (char*) task->data + task->size,
+                     .size = task->cap - task->size,
+                     .data = task,
+                     .callback = read_req_line_callback
+                 },
+                 .data = task->data,
+                 .size = task->size,
+                 .cap = task->cap
+            };
+    aio_scheduler_schedule((task_t*) task);
+}
 
 static void accept_connection(ssize_t r, int errno, void *udata) {
     task_t *task = udata;
@@ -18,6 +64,25 @@ static void accept_connection(ssize_t r, int errno, void *udata) {
         inet_ntop(AF_INET, &((struct sockaddr_in*) &connected_addr)->sin_addr, buff, INET_ADDRSTRLEN);
         fprintf(stderr, "[Info] Connected: %s\n", buff);
     }
+
+    vector_buffered_task_t *read_req_task = malloc(sizeof(vector_buffered_task_t));
+    void *buffer = malloc(CHUNK_SIZE); 
+    *read_req_task = (vector_buffered_task_t) 
+                     {
+                         .task = {
+                             .type = READ_REQUEST,
+                             .fd = fd,
+                             .buffer = buffer,
+                             .size = CHUNK_SIZE,
+                             .data = read_req_task,
+                             .callback = read_req_line_callback
+                         },
+                         .data = buffer,
+                         .size = 0,
+                         .cap = CHUNK_SIZE
+                     };
+
+    aio_scheduler_schedule((task_t*) read_req_task);
 }
 
 
@@ -27,6 +92,7 @@ void start_proxy(struct in_addr ip, in_port_t port) {
                                .sin_port = port,
                                .sin_addr = ip};
 
+    // TODO: Propagate error
     if (bind(listening, (struct sockaddr*) &addr, sizeof(addr))) {
         perror("Failed to bind");
         return;
@@ -41,7 +107,6 @@ void start_proxy(struct in_addr ip, in_port_t port) {
                               .type = ACCEPT_CONNECTION_REQUESTS,
                               .fd = listening,
                               .buffer = NULL,
-                              .off = 0,
                               .size = 0,
                               .data = &accept_task,
                               .callback = accept_connection
