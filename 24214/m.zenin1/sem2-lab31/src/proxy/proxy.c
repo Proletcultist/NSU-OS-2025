d48e93074e33b77d8763a3f1c7b8bf587452d32b
Matvey Zenin <m.zenin1@g.nsu.ru>
2026-04-28 15:14:53 +0700

    Refactor http state machine

--- 24214/m.zenin1/sem2-lab31/src/proxy/proxy.c
+++ 24214/m.zenin1/sem2-lab31/src/proxy/proxy.c
@@ -8,7 +8,7 @@
 #include <sys/types.h>
 #include <stddef.h>
 #include "scheduler/aio_scheduler.h"
-#include "proxy/request_analysis.h"
+#include "http.h"
 #include "proxy/responses.h"
 
 #define MAX_LINE_SIZE (8 * 1024)        // 8KB
@@ -19,7 +19,7 @@ typedef struct request_analysis_task {
     char client_ip[16];
     bool cacheable;
     size_t content_size;
-    request_analysis_data_t analysis_data;
+    http_state_machine_t sm;
 } request_analysis_task_t;
 
 static void write_response_callback(ssize_t r, int errno, void *udata) {
@@ -59,7 +59,7 @@ static void read_req_line_and_headers_callback(ssize_t r, int errno, void *udata
         fprintf(stderr, "[Error] Error while trying to read from %s: %s\n", task->client_ip, strerror(errno));
 
         close(task->task.fd);
-        request_analysis_data_t_destruct(&task->analysis_data);
+        http_state_machine_destruct(&task->sm);
         free(task);
         return;
     }
@@ -67,32 +67,32 @@ static void read_req_line_and_headers_callback(ssize_t r, int errno, void *udata
         fprintf(stderr, "[Error] Client %s terminated connection\n", task->client_ip);
 
         close(task->task.fd);
-        request_analysis_data_t_destruct(&task->analysis_data);
+        http_state_machine_destruct(&task->sm);
         free(task);
         return;
     }
 
-    char *eol = memchr(task->analysis_data.data.arr + task->analysis_data.data.size, '\n', (size_t) r);
+    char *eol = memchr(task->sm.data.arr + task->sm.data.size, '\n', (size_t) r);
 
     // No EOL found and max line size exceeded
-    if (eol == NULL && task->analysis_data.data.size - task->analysis_data.analyzed >= MAX_LINE_SIZE) {
-        request_analysis_data_t_destruct(&task->analysis_data);
+    if (eol == NULL && task->sm.data.size - task->sm.analyzed >= MAX_LINE_SIZE) {
+        http_state_machine_destruct(&task->sm);
         schedule_write_response(task->task.fd, too_long_line_response, too_long_line_response_size);
         free(task);
         return;
     }
     while (eol != NULL) {
-        try_analyze_next_line(&task->analysis_data);
+        http_state_machine_analyze_next_line(&task->sm);
 
-        switch (task->analysis_data.state) {
+        switch (task->sm.state) {
             case MALFORMED:
-                request_analysis_data_t_destruct(&task->analysis_data);
+                http_state_machine_destruct(&task->sm);
                 schedule_write_response(task->task.fd, bad_request_response, bad_request_response_size);
                 free(task);
                 return;
             case READ_REQUEST_LINE:
-                fprintf(stderr, "[Info] %s Parsed hostname: \"%s\" port: \"%s\", path: \"%s\"\n", task->client_ip, task->analysis_data.uri.hostname, task->analysis_data.uri.port, task->analysis_data.uri.path);
-                switch (task->analysis_data.method) {
+                fprintf(stderr, "[Info] %s Parsed hostname: \"%s\" port: \"%s\", path: \"%s\"\n", task->client_ip, task->sm.uri.hostname, task->sm.uri.port, task->sm.uri.path);
+                switch (task->sm.method) {
                     case GET:
                         task->cacheable = true;
                         break;
@@ -101,13 +101,13 @@ static void read_req_line_and_headers_callback(ssize_t r, int errno, void *udata
                         task->cacheable = false;
                         break;
                     case UNKNOWN_METHOD:
-                        request_analysis_data_t_destruct(&task->analysis_data);
+                        http_state_machine_destruct(&task->sm);
                         schedule_write_response(task->task.fd, method_not_implemented_response, method_not_implemented_response_size);
                         free(task);
                         return;
                 }
-                if (task->analysis_data.version != HTTP_1_0) {
-                    request_analysis_data_t_destruct(&task->analysis_data);
+                if (task->sm.version != HTTP_1_0) {
+                    http_state_machine_destruct(&task->sm);
                     schedule_write_response(task->task.fd, version_not_supported_response, version_not_supported_response_size);
                     free(task);
                     return;
@@ -119,35 +119,37 @@ static void read_req_line_and_headers_callback(ssize_t r, int errno, void *udata
             case COMPLETE:
                 // TODO: Open connection with server, schedule writing to it
                 return;
-
+            case READING_REQUEST_LINE:
+            case READING_HEADERS:
+                break;
         }
 
-        eol = memchr(eol + 1, '\n', (size_t) r - (size_t) (eol - (task->analysis_data.data.arr + task->analysis_data.data.size)) - 1);
+        eol = memchr(eol + 1, '\n', (size_t) r - (size_t) (eol - (task->sm.data.arr + task->sm.data.size)) - 1);
     }
-    task->analysis_data.data.size += (size_t) r;
+    task->sm.data.size += (size_t) r;
 
 
     printf("\nBuffer content:\n");
     printf("\033[32m");
-    for (size_t i = 0; i < task->analysis_data.analyzed; i++) {
-        printf("%c", task->analysis_data.data.arr[i]);
+    for (size_t i = 0; i < task->sm.analyzed; i++) {
+        printf("%c", task->sm.data.arr[i]);
     }
     printf("\033[0m");
-    for (size_t i = task->analysis_data.analyzed; i < task->analysis_data.data.size; i++) {
-        printf("%c", task->analysis_data.data.arr[i]);
+    for (size_t i = task->sm.analyzed; i < task->sm.data.size; i++) {
+        printf("%c", task->sm.data.arr[i]);
     }
     printf("\n");
 
 
-    if (task->analysis_data.data.size == task->analysis_data.data.cap) {
-        vector_char_t_reserve(&task->analysis_data.data, task->analysis_data.data.cap + MAX_LINE_SIZE);
+    if (task->sm.data.size == task->sm.data.cap) {
+        vector_char_t_reserve(&task->sm.data, task->sm.data.cap + MAX_LINE_SIZE);
     }
     task->task = (task_t)
                  {
                      .type = READ_REQUEST,
                      .fd = task->task.fd,
-                     .buffer = task->analysis_data.data.arr + task->analysis_data.data.size,
-                     .size = task->analysis_data.data.cap - task->analysis_data.data.size,
+                     .buffer = task->sm.data.arr + task->sm.data.size,
+                     .size = task->sm.data.cap - task->sm.data.size,
                      .data = task,
                      .callback = read_req_line_and_headers_callback
                  };
@@ -171,15 +173,15 @@ static void accept_connection(ssize_t r, int errno, void *udata) {
         strncpy(read_req_task->client_ip, "Unknown", sizeof(read_req_task->client_ip) - 1);
     }
 
-    read_req_task->analysis_data = REQUEST_ANALYSIS_DATA_INITIALIZER;
-    vector_char_t_reserve(&read_req_task->analysis_data.data, MAX_LINE_SIZE);
+    read_req_task->sm = HTTP_STATE_MACHINE_INITIALIZER;
+    vector_char_t_reserve(&read_req_task->sm.data, MAX_LINE_SIZE);
 
     read_req_task->task = (task_t)
                           {
                               .type = READ_REQUEST,
                               .fd = fd,
-                              .buffer = read_req_task->analysis_data.data.arr,
-                              .size = read_req_task->analysis_data.data.cap,
+                              .buffer = read_req_task->sm.data.arr,
+                              .size = read_req_task->sm.data.cap,
                               .data = read_req_task,
                               .callback = read_req_line_and_headers_callback
                           };
