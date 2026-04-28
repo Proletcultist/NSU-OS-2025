2bed885f03b2dec674698fdcdf97e3ff6efe9c50
Matvey Zenin <m.zenin1@g.nsu.ru>
2026-04-28 14:56:29 +0700

    Make analysis data be state machine

--- 24214/m.zenin1/sem2-lab31/include/proxy/request_analysis.h
+++ 24214/m.zenin1/sem2-lab31/include/proxy/request_analysis.h
@@ -11,26 +11,19 @@
 #undef TYPE
 #undef NAME
 
-#define URI_INITIALIZER ((uri_t) \
-                         { \
-                          .buffer = NULL, \
-                          .hostname = NULL, \
-                          .port = NULL, \
-                          .path = NULL, \
-                         })
-
 #define REQUEST_ANALYSIS_DATA_INITIALIZER ((request_analysis_data_t) \
                                            { \
                                             .state = READING_REQUEST_LINE, \
-                                            .uri = URI_INITIALIZER, \
-                                            .cacheable = false, \
-                                            .content_size = 0, \
                                             .analyzed = 0, \
                                             .data = (vector_char_t) VECTOR_INITIALIZER \
                                            })
 typedef enum request_analysis_state {
     READING_REQUEST_LINE,
-    READING_HEADERS
+    READ_REQUEST_LINE,
+    READING_HEADERS,
+    HEADER_AVAILABLE,
+    MALFORMED,
+    COMPLETE
 } request_analysis_state_t;
 
 typedef struct uri {
@@ -40,25 +33,36 @@ typedef struct uri {
     char *path;
 } uri_t;
 
+typedef struct header {
+    char *name;
+    char *value;
+    size_t name_size, value_size;
+} header_t;
+
+typedef enum http_method {
+    GET,
+    POST,
+    HEAD,
+    UNKNOWN_METHOD
+} http_method_t;
+
+typedef enum http_version {
+    HTTP_1_0,
+    HTTP_1_1
+} http_version_t;
+
 typedef struct request_analysis_data {
     request_analysis_state_t state;   
+    http_method_t method;
     uri_t uri;
-    bool cacheable;
-    size_t content_size;
+    http_version_t version;
+    header_t last_header;
 
     size_t analyzed;
     vector_char_t data;
 } request_analysis_data_t;
 
-typedef enum request_analysis_result {
-    INCOMPLETE,             // Request + headers not ended yet
-    MALFORMED,              // Bad request (400)
-    METHOD_NOT_IMPLEMENTED, // HTTP 501
-    VERSION_NOT_SUPPORTED,  // HTTP 505
-    COMPLETE                // Request + headers all read, can start transfer body if there is any
-} request_analysis_result_t;
-
-request_analysis_result_t try_analyze_req_line(request_analysis_data_t *data);
-request_analysis_result_t try_analyze_header(request_analysis_data_t *data);
-request_analysis_result_t try_analyze_next_line(request_analysis_data_t *data);
+void try_analyze_req_line(request_analysis_data_t *data);
+void try_analyze_header(request_analysis_data_t *data);
+void try_analyze_next_line(request_analysis_data_t *data);
 void request_analysis_data_t_destruct(request_analysis_data_t *data);
