8621ce0f70028005bc5d6d6b98c44ae3eb45e197
Matvey Zenin <m.zenin1@g.nsu.ru>
2026-04-30 08:55:12 +0700

    Refactor http state machine

--- 24214/m.zenin1/sem2-lab31/src/http.c
+++ 24214/m.zenin1/sem2-lab31/src/http.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <ctype.h>
 #include <stdbool.h>
 #include <string.h>
 #include "http.h"
@@ -7,6 +8,103 @@
                        (c) == ',' || (c) == ';' || (c) == ':' || (c) == '\\' || (c) == '"' || \
                        (c) == '/' || (c) == '[' || (c) == ']' || (c) == '?' || (c) == '=' || \
                        (c) == '{' || (c) ==  '}' || (c) == ' ' || (c) == '\t')
+
+// TODO: May be also handle escape (% HEX HEX)
+#define isuchar(c) (isalnum(c) || (c) == '$' || (c) == '-' || (c) == '_' || (c) == '.' || \
+                    (c) == '!' || (c) == '*' || (c) == '\'' || (c) == '(' || (c) == ')' || (c) == ',')
+
+#define isreserved(c) ((c) == ';' || (c) == '/' || (c) == '?' || (c) == ':' || (c) == '@' || \
+                       (c) == '&' || (c) == '=' || (c) == '+')
+
+#define ispchar(c) ((c) == ':' || (c) == '@' || (c) == '&' || (c) == '=' || (c) == '+' || isuchar(c))
+
+static bool skip_word(char **cursor, http_state_machine_t *sm) {
+    if (**cursor == '"') {
+        // TODO: Quoted words
+    }
+    else {
+        if (istspecial(**cursor) || iscntrl(**cursor)) {
+            sm->state = MALFORMED;
+            return false;
+        }
+        while (!istspecial(**cursor) && !iscntrl(**cursor)) {
+            (*cursor)++;
+        }
+    }
+
+    return true;
+}
+
+static bool skip_hostname(char **cursor, http_state_machine_t *sm) {
+    while (true) {
+        if (!isalnum(**cursor)) {
+            sm->state = MALFORMED;
+            return false;
+        }
+        while (isalnum(**cursor) || **cursor == '-') {
+            (*cursor)++;
+        }
+
+        if (**cursor == '-') {
+            sm->state = MALFORMED;
+            return false;
+        }
+
+        if (**cursor != '.') {
+            break;
+        }
+
+        (*cursor)++;
+    }
+
+    return true;
+}
+
+static bool skip_number(char **cursor, http_state_machine_t *sm) {
+    if (!isdigit(**cursor)) {
+        sm->state = MALFORMED;
+        return false;
+    }
+    while (isdigit(**cursor)) {
+        (*cursor)++;
+    }
+
+    return true;
+}
+
+static bool skip_abs_path(char **cursor, http_state_machine_t *sm) {
+    // Slash at the beginning
+    if (**cursor != '/') {
+        sm->state = MALFORMED;
+        return false;
+    }
+    (*cursor)++;
+
+    // Path
+    if (ispchar(**cursor)) {
+        while (ispchar(**cursor) || **cursor == '/') {
+            (*cursor)++;
+        }
+    }
+
+    // Params
+    if (**cursor == ';') {
+        (*cursor)++;
+        while (ispchar(**cursor) || **cursor == '/' || **cursor == ';') {
+            (*cursor)++;
+        }
+    }
+
+    // Query
+    if (**cursor == '?') {
+        (*cursor)++;
+        while (isuchar(**cursor) || isreserved(**cursor)) {
+            (*cursor)++;
+        }
+    }
+
+    return true;
+}
 
 // strncmp but for http lines
 // \0, \r\n and \n are considered as line ends
@@ -26,111 +124,154 @@ static bool linencmp(char *s1, char *s2, size_t n) {
     return true;
 }
 
-void analyze_req_line(http_state_machine_t *sm) {
-    char *cursor = sm->data.arr + sm->analyzed;
-
+static bool analyze_method(char **cursor, http_state_machine_t *sm) {
     // Checking method
-    if (linencmp(cursor, "GET ", 4)) {
-        cursor += 3;
+    if (linencmp(*cursor, "GET ", 4)) {
+        (*cursor) += 3;
         sm->method = GET;
     }
-    else if (linencmp(cursor, "POST ", 5)) {
-        cursor += 4;
+    else if (linencmp(*cursor, "POST ", 5)) {
+        (*cursor) += 4;
         sm->method = POST;
     }
-    else if (linencmp(cursor, "HEAD ", 5)) {
-        cursor += 4;
+    else if (linencmp(*cursor, "HEAD ", 5)) {
+        (*cursor) += 4;
         sm->method = HEAD;
     }
     else {
-        if (istspecial(*cursor) || *cursor == '\n' || *cursor == '\r') {
-            sm->state = MALFORMED;
-            return;
-        }
-        size_t space_dist = strcspn(cursor, " \r\n");
-        cursor += space_dist;
-        if (*cursor != ' ') {
-            sm->state = MALFORMED;
-            return;
+        if (!skip_word(cursor, sm)) {
+            return false;
         }
         sm->method = UNKNOWN_METHOD;
     }
 
-    cursor += strspn(cursor, " ");
-    if (*cursor == '\r' || *cursor == '\n') {
-        sm->state = MALFORMED;
-        return;
-    }
+    return true;
+}
+
+static bool analyze_uri(char **cursor, http_state_machine_t *sm,
+                        char **hostname, size_t *hostname_size,
+                        char **port, size_t *port_size,
+                        char **path, size_t *path_size) {
+    *port_size = 0;
+    *path_size = 0;
 
     // Parsing URI into hostname and port
-    if (!linencmp(cursor, "http://", 7)) {
+    if (!linencmp(*cursor, "http://", 7)) {
         sm->state = MALFORMED;
-        return;
+        return false;
     }
-    cursor += 7;
+    (*cursor) += 7;
 
-    char *hostname = cursor;
-    size_t hostname_size = strcspn(cursor, ":/ \n");
-    char *port = NULL;
-    char *path = NULL;
-    size_t port_size = 0, path_size = 0;
+    *hostname = *cursor;
+    if (!skip_hostname(cursor, sm)) {
+        return false;
+    }
+    *hostname_size = (size_t) (*cursor - *hostname);
 
-    cursor += hostname_size;
-    if (*cursor == ':') {
-        cursor++;
-        port = cursor;
-        port_size = strspn(cursor, "0123456789");
-        cursor += port_size;
-        if (*cursor != ' ' && *cursor != '/') {
-            sm->state = MALFORMED;
-            return;
+    if (**cursor == ':') {
+        (*cursor)++;
+        *port = *cursor;
+        if (!skip_number(cursor, sm)) {
+            return false;
         }
+        *port_size = (size_t) (*cursor - *port);
     }
 
-    if (*cursor == '\r' || *cursor == '\n') {
-        sm->state = MALFORMED;
-        return;
-    }
-    path = cursor;
-    path_size = strcspn(cursor, " \n");
-    cursor += path_size;
-    cursor += strspn(cursor, " ");
-
-    if (*cursor == '\r' || *cursor == '\n') {
-        sm->state = MALFORMED;
-        return;
+    if (**cursor == '/') {
+        *path = *cursor;
+        if (!skip_abs_path(cursor, sm)) {
+                return false;
+        }
+        *path_size = (size_t) (*cursor - *path);
     }
 
+    return true;
+}
+
+static bool analyze_version(char **cursor, http_state_machine_t *sm) {
     // Checking HTTP version
-    if (linencmp(cursor, "HTTP/1.0", 8)) {
-        cursor += 8;
+    if (linencmp(*cursor, "HTTP/1.0", 8)) {
+        (*cursor) += 8;
         sm->version = HTTP_1_0;
     }
-    else if (linencmp(cursor, "HTTP/1.1", 8)) {
-        cursor += 8;
+    else if (linencmp(*cursor, "HTTP/1.1", 8)) {
+        (*cursor) += 8;
         sm->version = HTTP_1_1;
     }
     else {
         sm->state = MALFORMED;
+        return false;
+    }
+
+    return true;
+}
+
+static bool skip_spaces(char **cursor, http_state_machine_t *sm) {
+    if (**cursor != ' ') {
+        sm->state = MALFORMED;
+        return false;
+    }
+    while (**cursor == ' ') {
+        (*cursor)++;
+    }
+
+    return true;
+}
+
+static bool skip_eol(char **cursor, http_state_machine_t *sm) {
+    if (**cursor == '\n') {
+        (*cursor)++;
+        return true;
+    }
+
+    if (**cursor != '\r') {
+        sm->state = MALFORMED;
+        return false;
+    }
+    (*cursor)++;
+
+    if (**cursor != '\n') {
+        sm->state = MALFORMED;
+        return false;
+    }
+    (*cursor)++;
+
+    return true;
+}
+
+void analyze_req_line(http_state_machine_t *sm) {
+    char *cursor = sm->data.arr + sm->analyzed;
+
+    if (!analyze_method(&cursor, sm)) {
         return;
     }
 
-    if (*cursor != ' ' && !(*cursor == '\r' && *(cursor + 1) == '\n') && *cursor != '\n') {
-        sm->state = MALFORMED;
+    if (!skip_spaces(&cursor, sm)) {
         return;
     }
 
-    cursor += strspn(cursor, " ");
-    if (!(*cursor == '\r' && *(cursor + 1) == '\n') && *cursor != '\n') {
-        sm->state = MALFORMED;
+    char *hostname, *port, *path;
+    size_t hostname_size, port_size, path_size;
+    if (!analyze_uri(&cursor, sm,
+                     &hostname, &hostname_size,
+                     &port, &port_size,
+                     &path, &path_size)) {
         return;
     }
-    
-    // Placing cursor right on the LF
-    if (*cursor == '\r') {
-        cursor++;
+
+    if (!skip_spaces(&cursor, sm)) {
+        return;
     }
-    sm->analyzed = cursor -(sm->data.arr + sm->analyzed);
+
+    if (!analyze_version(&cursor, sm)) {
+        return;
+    }
+
+    if (!skip_spaces(&cursor, sm) && !skip_eol(&cursor, sm)) {
+        return;
+    }
+
+    sm->analyzed = cursor - sm->data.arr;
 
     sm->uri.buffer = malloc(hostname_size + 1 + port_size + 1 + path_size + 1);
 
