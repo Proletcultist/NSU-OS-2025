f4b579a6cf6f7779e446471c58338a0eb583d3dc
Дешин Михаил Александрович <114480953+DeshinMichael@users.noreply.github.com>
2026-05-26 02:06:58 +0700

    did sem2-lab4 (#853)

---
+++ 24214/m.deshin/sem2-lab4/lab4.c
@@ -0,0 +1,44 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+static void die_pthread(int err, const char *what) {
+    if (err != 0) {
+        fprintf(stderr, "%s: %s\n", what, strerror(err));
+        exit(EXIT_FAILURE);
+    }
+}
+
+static void *worker(void *arg) {
+    (void)arg;
+    while (1) {
+        sleep(1);
+        printf("Child thread worked...\n");
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t t;
+
+    int err = pthread_create(&t, NULL, worker, NULL);
+    die_pthread(err, "pthread_create");
+
+    sleep(2);
+
+    err = pthread_cancel(t);
+    die_pthread(err, "pthread_cancel");
+
+    void *status;
+    err = pthread_join(t, &status);
+    die_pthread(err, "pthread_join");
+
+    if (status == PTHREAD_CANCELED) printf("Child thread was cancelled\n");
+    else printf("Child thread exited normally\n");
+
+    return EXIT_SUCCESS;
+}
